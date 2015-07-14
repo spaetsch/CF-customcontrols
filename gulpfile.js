a7892e2dc88f462f80dd00e5cc282c6a68f10ff0
Sarah Paetsch <spaetsch@gmail.com>
2015-07-14 12:10:35 -0700

    Add video styles, fixed gulpfile path for sass watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('./sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.sass', ['sass']);
 });
